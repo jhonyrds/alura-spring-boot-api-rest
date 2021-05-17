6c01c435aaef42562b4afdf3b8d9ec0e43c3fa72
Jhony de Oliveira Rodrigues <jhony.o_rodrigues@hotmail.com>
2021-05-17 17:59:03 -0300

    deploy com docker e heroku

--- src/test/java/br/com/alura/forum/ForumApplicationTests.java
+++ src/test/java/br/com/alura/forum/ForumApplicationTests.java
@@ -1,8 +1,6 @@
 package br.com.alura.forum;
 
 
-import org.junit.Assert;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -11,9 +9,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 class ForumApplicationTests {
 
-	@Test
-	void contextLoads() {
-		Assert.assertTrue(true);
-	}
+	
 
 }
--- src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
+++ src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
@@ -1,7 +1,8 @@
 package br.com.alura.forum.repository;
 
+
 import org.junit.Assert;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -11,6 +12,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import br.com.alura.forum.modelo.Curso;
+import br.com.alura.forum.repository.CursoRepository;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
